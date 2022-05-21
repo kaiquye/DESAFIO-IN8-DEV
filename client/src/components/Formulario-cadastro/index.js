6aaa3f6e13b553ca05464ee5a198a06a7ce0ee24
kaiquye <kaiquemendesilva82@gmail.com>
2022-05-21 17:22:14 -0300

    validacao do formulario

--- client/src/components/Formulario-cadastro/index.js
+++ client/src/components/Formulario-cadastro/index.js
@@ -1,41 +1,64 @@
-import { Input, TextField } from '@material-ui/core';
 import style from './cadatro.module.css'
+
+import { useForm } from "react-hook-form";
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from "yup";
+
+const ValidarCampos = yup.object({
+    nome: yup.string().required(),
+    email: yup.string().email().required(),
+    telefone: yup.string().required(),
+    nascimento: yup.date().required(),
+}).required();
+
+
 
 function NovoEstagiario() {
 
+    const { register, handleSubmit, formState: { errors } } = useForm({
+        resolver: yupResolver(ValidarCampos)
+    });
+    const onSubmit = data => console.log(data);
 
     return (
         <section className={style.cadastro} >
             <main className={style.cadastro_body} >
-                <h1>CADASTRO</h1>
-                <form className={style.cadastro_body_form} >
-                    <div>
+                <h1 className={style.cadastro_body_titulo} > CADASTRO</h1>
+                <form className={style.cadastro_body_form} onSubmit={handleSubmit(onSubmit)}  >
+                    <div className={style.cadastro_body_form_input}  >
                         <label>
-                            nome
+                            Nome
                         </label>
-                        <input />
+                        <input {...register("nome")} name='nome' type='text' />
+                        <p>{errors.nome?.message}</p>
                     </div>
-                    <div>
+                    <div className={style.cadastro_body_form_input}  >
                         <label>
                             E-mail
                         </label>
-                        <input />
+                        <input {...register("email")} name='email' type='text' />
+                        <p>{errors.email?.message}</p>
                     </div>
-                    <div>
+                    <div className={style.cadastro_body_form_input}  >
                         <label>
                             Nascimento
                         </label>
-                        <input />
+                        <input {...register("nascimento")} name='nascimento' type='text' />
+                        <p>{errors.nascimento?.message}</p>
                     </div>
-                    <div>
+                    <div className={style.cadastro_body_form_input}  >
                         <label>
                             Telefone
                         </label>
-                        <input />
+                        <input {...register("telefone")} name='telefone' type='text' />
+                        <p>{errors.telefone?.message}</p>
+                    </div>
+                    <div className={style.cadastro_body_form_btn} >
+                        <button className={style.cadastro_body_form_button}>CADASTRAR</button>
                     </div>
                 </form>
             </main>
-        </section>
+        </section >
     )
 
 }
